519b49e55067962ec295912b22c42299679c2736
devcow85 <devcow85@gmail.com>
2025-03-10 15:41:07 +0900

    chore(proj): remove examples

--- examples/profile_test.py
+++
@@ -1,8 +0,0 @@
-from torchevent import models
-
-
-if __name__ == "__main__":
-    model = models.NMNISTNet(5, 1, n_steps = 5)
-    trace_data = model.trace((5,2,34,34))
-    
-    model.summary((5,2,34,34))
